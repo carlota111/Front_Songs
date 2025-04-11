b8f6a1a0948d4539c8d57a482b73ec6ded3b0c34
ines <inesindev@gmail.com>
2025-04-11 13:21:16 +0200

    Cargados datos en tabla

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from Classes.DataLoader import DataJSONLoader, DataLoader, DataAPILoader
-
+import streamlit as st
+import pandas as pd 
 data_loader = DataLoader(DataJSONLoader("Data/short_cleaned_songs.json"))
-print("Datos desde JSON:", data_loader.load())
+datos=data_loader.load()
+
+df = pd.DataFrame(datos['datos_limpios'])
+
+st.write("Tabla de datos:")
+st.dataframe(df, use_container_width=True) 
